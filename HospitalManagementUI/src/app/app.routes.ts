0004308642d9c59d34777c5a7c985ff2de313ca6
Pratibha Maurya <Pratibha@veersatech.com>
2026-07-23 13:53:00 +0530

    Login page UI complete

--- HospitalManagementUI/src/app/app.routes.ts
+++ HospitalManagementUI/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
-import { Register } from './components/patient/register/register';
+import { Register } from './components/auth/register/register';
+import { Login } from './components/auth/login/login';
 
 export const routes: Routes = [
   {
     path: '',
-    component: Register
+    redirectTo:'login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:Login
+  },
+  {
+    path:'register',
+    component:Register
   }
 ];
