85c77d07df10e0720b0cb733f61d844e19ff58ee
Pratibha Maurya <Pratibha@veersatech.com>
2026-08-03 13:55:36 +0530

    Resolve issue raise because of Deleteing Emails from Patient Table

---
+++ HospitalManagementAPI/Migrations/20260803063113_RemoveEmailFromPatient.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEmailFromPatient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Patients");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
